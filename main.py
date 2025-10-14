35bdef6f3241c86255ac620b45622aee180d72fd
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-10-14 17:26:30 +0100

    ..

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from textual.app import App
+from textual import events
+from textual.app import App, ComposeResult
+from textual.widgets import RichLog
 
 
-class MyApp(App):
-    pass
+class InputApp(App):
+
+    def compose(self) -> ComposeResult:
+        yield RichLog()
+
+    def on_key(self, event: events.Key) -> None:
+        self.query_one(RichLog).write(event)
 
 
 if __name__ == "__main__":
-    app = MyApp()
+    app = InputApp()
     app.run()
