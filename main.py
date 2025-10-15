cea851f60983bc88647e4d0cf5bec00883685ebe
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-10-15 14:18:47 +0100

    tinkering around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, Vertical
+from textual.containers import Horizontal
 from textual.widgets import Header, Footer, Button, Static
 
 class TypingTest(App):
@@ -10,10 +10,19 @@ class TypingTest(App):
     
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Static("[bold]Welcome![bold]\nSelect a game mode below to start typing", id="Welcome")
+        yield Static("""
+____    __    ____  _______  __        ______   ______   .___  ___.  _______ 
+\   \  /  \  /   / |   ____||  |      /      | /  __  \  |   \/   | |   ____|
+ \   \/    \/   /  |  |__   |  |     |  ,----'|  |  |  | |  \  /  | |  |__   
+  \            /   |   __|  |  |     |  |     |  |  |  | |  |\/|  | |   __|  
+   \    /\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____ 
+    \__/  \__/     |_______||_______| \______| \______/  |__|  |__| |_______|
+                                                                             
+please Choose a Game Mode""", id="welcome")
         yield Horizontal(
             Button("Single Player"),
             Button("Multi Player"),
+            id="menu"
         )
         yield Footer()
 
